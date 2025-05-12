21894cb24dee167da2894be6b19a9491977738b6
ERNESTOURIELTORRES <2005eugt@gmail.com>
2025-05-12 12:41:43 -0600

    5 puntos final

--- TEMA 4/Diferenciacion5Puntos.java
+++ TEMA 4/Diferenciacion5Puntos.java
@@ -28,6 +28,87 @@ public class Diferenciacion5Puntos {
         System.out.println("\nRESULTADO:");
         System.out.printf("f'(%.4f) ≈ %.12f%n", x0, derivada);
         System.out.println("Fórmula utilizada: [-f(x+2h) + 8f(x+h) - 8f(x-h) + f(x-2h)] / (12h)");
+    }  // Método evaluarFuncion() idéntico al de la clase Diferenciacion3Puntos
+    private static double evaluarFuncion(String expr) {
+        // Implementación básica de evaluación de expresiones
+        try {
+            return new Object() {
+                int pos = -1, ch;
+                
+                void nextChar() {
+                    ch = (++pos < expr.length()) ? expr.charAt(pos) : -1;
+                }
+                
+                boolean eat(int charToEat) {
+                    while (ch == ' ') nextChar();
+                    if (ch == charToEat) {
+                        nextChar();
+                        return true;
+                    }
+                    return false;
+                }
+                
+                double parse() {
+                    nextChar();
+                    double x = parseExpression();
+                    if (pos < expr.length()) throw new RuntimeException("Carácter inesperado: " + (char)ch);
+                    return x;
+                }
+                
+                double parseExpression() {
+                    double x = parseTerm();
+                    for (;;) {
+                        if (eat('+')) x += parseTerm();
+                        else if (eat('-')) x -= parseTerm();
+                        else return x;
+                    }
+                }
+                
+                double parseTerm() {
+                    double x = parseFactor();
+                    for (;;) {
+                        if (eat('*')) x *= parseFactor();
+                        else if (eat('/')) x /= parseFactor();
+                        else if (eat('^')) x = Math.pow(x, parseFactor());
+                        else return x;
+                    }
+                }
+                
+                double parseFactor() {
+                    if (eat('+')) return parseFactor();
+                    if (eat('-')) return -parseFactor();
+                    
+                    double x;
+                    int startPos = this.pos;
+                    if (eat('(')) {
+                        x = parseExpression();
+                        eat(')');
+                    } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                        while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                        x = Double.parseDouble(expr.substring(startPos, this.pos));
+                    } else if (ch >= 'a' && ch <= 'z') {
+                        while (ch >= 'a' && ch <= 'z') nextChar();
+                        String func = expr.substring(startPos, this.pos);
+                        x = parseFactor();
+                        switch (func) {
+                            case "sqrt": x = Math.sqrt(x); break;
+                            case "sin": x = Math.sin(x); break;
+                            case "cos": x = Math.cos(x); break;
+                            case "tan": x = Math.tan(x); break;
+                            case "log": x = Math.log(x); break;
+                            case "exp": x = Math.exp(x); break;
+                            default: throw new RuntimeException("Función desconocida: " + func);
+                        }
+                    } else {
+                        throw new RuntimeException("Carácter inesperado: " + (char)ch);
+                    }
+                    return x;
+                }
+            }.parse();
+        } catch (Exception e) {
+            throw new RuntimeException("Error al evaluar la función: " + e.getMessage());
+        }
     }
+}
     
   
