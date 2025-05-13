027d7d956a8d40f4d35dfc983fb6c4643ba63def
ERNESTOURIELTORRES <2005eugt@gmail.com>
2025-05-12 19:50:55 -0600

    Avance final Integracion Trapecio

--- TEMA 4/IntegracionTrapecio.java
+++ TEMA 4/IntegracionTrapecio.java
@@ -24,6 +24,104 @@ class EntradaDatosTrapecio {
         
         System.out.println("Introduce el número de intervalos (n):");
         this.n = scanner.nextInt();
+    }private double evaluarExpresion(String expr) {
+        // Implementación básica de evaluación de expresiones
+        try {
+            return new Object() {
+                int pos = -1, ch;
+                
+                void nextChar() {
+                    ch = (++pos < expr.length()) ? expr.charAt(pos) : -1;
+                }
+                
+                boolean eat(int charToEat) {
+                    while (ch == ' ') nextChar();
+                    if (ch == charToEat) {
+                        nextChar();
+                        return true;
+                    }
+                    return false;
+                }
+                
+                double parse() {
+                    nextChar();
+                    double x = parseExpression();
+                    if (pos < expr.length()) throw new RuntimeException("Carácter inesperado: " + (char)ch);
+                    return x;
+                }
+                
+                double parseExpression() {
+                    double x = parseTerm();
+                    for (;;) {
+                        if (eat('+')) x += parseTerm();
+                        else if (eat('-')) x -= parseTerm();
+                        else return x;
+                    }
+                }
+                
+                double parseTerm() {
+                    double x = parseFactor();
+                    for (;;) {
+                        if (eat('*')) x *= parseFactor();
+                        else if (eat('/')) x /= parseFactor();
+                        else if (eat('^')) x = Math.pow(x, parseFactor());
+                        else return x;
+                    }
+                }
+                
+                double parseFactor() {
+                    if (eat('+')) return parseFactor();
+                    if (eat('-')) return -parseFactor();
+                    
+                    double x;
+                    int startPos = this.pos;
+                    if (eat('(')) {
+                        x = parseExpression();
+                        eat(')');
+                    } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                        while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                        x = Double.parseDouble(expr.substring(startPos, this.pos));
+                    } else if (ch >= 'a' && ch <= 'z') {
+                        while (ch >= 'a' && ch <= 'z') nextChar();
+                        String func = expr.substring(startPos, this.pos);
+                        x = parseFactor();
+                        switch (func) {
+                            case "sqrt": x = Math.sqrt(x); break;
+                            case "sin": x = Math.sin(x); break;
+                            case "cos": x = Math.cos(x); break;
+                            case "tan": x = Math.tan(x); break;
+                            case "log": x = Math.log(x); break;
+                            case "exp": x = Math.exp(x); break;
+                            default: throw new RuntimeException("Función desconocida: " + func);
+                        }
+                    } else {
+                        throw new RuntimeException("Carácter inesperado: " + (char)ch);
+                    }
+                    
+                    return x;
+                }
+            }.parse();
+        } catch (Exception e) {
+            throw new RuntimeException("Error al evaluar la función: " + e.getMessage());
+        }
     }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public Function<Double, Double> getFuncion() {
+        return funcion;
+    }
+}
+
     
     
