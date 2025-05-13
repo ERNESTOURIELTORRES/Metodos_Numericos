c0de4dbd84b7fa437708fce622e6e9dd859e9bfb
ERNESTOURIELTORRES <2005eugt@gmail.com>
2025-05-12 22:53:34 -0600

    Avance final NewtonRaphson

--- TEMA 2/NewtonRaphson.java
+++ TEMA 2/NewtonRaphson.java
@@ -7,4 +7,15 @@ public class NewtonRaphson {
         return 3 * Math.pow(x, 2) - 1;
     
     }
-    
+    public static void main(String[] args) {
+        double x = 1.5, tolerancia = 0.0001;
+        double h;
+
+        do {
+            h = funcion(x) / derivada(x);
+            x = x - h;
+        } while (Math.abs(h) >= tolerancia);
+
+        System.out.println("La raíz aproximada es: " + x);
+    }
+}
