331fa0691c56506fb63a3e7da310c76b1f244816
ERNESTOURIELTORRES <2005eugt@gmail.com>
2025-05-12 20:30:23 -0600

    Bioseccion avance

---
+++ TEMA 2/Biseccion.java
@@ -0,0 +1,15 @@
+
+public class Biseccion {
+    public static double funcion(double x) {
+        return Math.pow(x, 3) - x - 2;
+    }
+
+    public static void main(String[] args) {
+        double a = 1, b = 2, tolerancia = 0.0001;
+        double c;
+
+        if (funcion(a) * funcion(b) >= 0) {
+            System.out.println("No se puede aplicar el método.");
+            return;
+        }
+}
