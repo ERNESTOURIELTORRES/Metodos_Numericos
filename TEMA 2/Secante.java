db599d6a23ef720115f57adcf260d51eb96b8e8e
ERNESTOURIELTORRES <2005eugt@gmail.com>
2025-05-13 22:49:40 -0600

    Secante avance final

--- TEMA 2/Secante.java
+++ TEMA 2/Secante.java
@@ -6,3 +6,13 @@ public class Secante {
     public static void main(String[] args) {
         double x0 = 1, x1 = 2, tolerancia = 0.0001;
         double x2;
+
+           do {
+            x2 = x1 - funcion(x1) * (x1 - x0) / (funcion(x1) - funcion(x0));
+            x0 = x1;
+            x1 = x2;
+        } while (Math.abs(funcion(x2)) >= tolerancia);
+
+        System.out.println("La raíz aproximada es: " + x2);
+    }
+}
