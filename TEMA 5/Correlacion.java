31a4432f5b7341c2ee713a718bc44c90fc0f5a68
ERNESTOURIELTORRES <2005eugt@gmail.com>
2025-05-18 00:52:57 -0600

    correlacion

---
+++ TEMA 5/Correlacion.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; 
+
+public class Correlacion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
+        int n; 
+        double xi, yi;
+        double sumaX = 0, sumaY = 0, sumaXY = 0, sumaX2 = 0, sumaY2 = 0;
+        double coefPendiente, coefIntercepto, r;
+
+        System.out.print("Ingrese el número de datos: ");
+        n = scanner.nextInt();
+
+      
+        double[] datosX = new double[n];
+        double[] datosY = new double[n];
+
+       
+        
+}
+
+
+ 
